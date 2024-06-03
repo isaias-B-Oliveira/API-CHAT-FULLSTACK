23e354c6a5d010966b0140f01404ec66b572ffca
isaias-B-Oliveira <joseisaias123bezerra@gmail.com>
2024-06-03 18:35:12 -0300

    config de dependecia socket.io | v1 min 28:11

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
 const express = require("express");
+const socket = require("socket.io");
+const cors = require("cors");
 const app = express();
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(" Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+    res.header(
+        " Access-Control-Allow-Headers",
+        "X-PINGOTHER, Content-Type, Authorization"
+    );
+    app.use(cors());
+    next();
+});
 
 app.get("/", function (req, res) {
     res.send("primeira rota");
 });
 
-app.listen(8080, () => {
+const serve = app.listen(8080, () => {
     console.log("serve running : http://localhost:8080/");
+});
+
+io = socket(serve, { cors: { origin: "*" } });
+
+io.on("connection", (socket) => {
+    console.log(socket.id);
 });
 
 //video 1 min 18:16
