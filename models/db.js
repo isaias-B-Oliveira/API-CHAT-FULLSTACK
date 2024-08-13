4355fb670a503e23c777e5f4abe4f81c258933bb
isaias-B-Oliveira <joseisaias123bezerra@gmail.com>
2024-08-13 18:21:49 -0300

    configurando as variaveis de ambiente com .env

--- models/db.js
+++ models/db.js
@@ -1,17 +1,23 @@
+require("dotenv").config();
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("chat", "isaias", "91827364", {
-    host: "172.18.44.28", // ou 'localhost'
-    dialect: "mysql",
-});
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    {
+        host: process.env.DB_HOST,
+        dialect: process.env.DB_DIALECT,
+    }
+);
 
 sequelize
     .authenticate()
     .then(() => {
-        console.log("coneção com sql_serve bem sucedida");
+        console.log("Conexão com SQL Server bem sucedida");
     })
     .catch((err) => {
-        console.log("sem conexão com db", err);
+        console.log("Sem conexão com DB", err);
     });
 
 module.exports = sequelize;
